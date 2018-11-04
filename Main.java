f56d710e18e5e55dc92a2434ff732898b0623812
Дарья Борисова <d.borisova@MacBook-Pro-Dara.local>
2018-11-04 20:14:45 +0300

    Main.java
    Lesson_one_zadanie2

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+       StringBuffer pereverni = new StringBuffer("переверни строку");
+        pereverni.reverse();
+
+        System.out.println(pereverni);
+    }
+}
