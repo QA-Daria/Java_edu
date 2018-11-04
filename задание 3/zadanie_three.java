ffcbba4519b12da87abf2c3dafb2c4b1d0763961
Дарья Борисова <d.borisova@MacBook-Pro-Dara.local>
2018-11-04 22:01:13 +0300

    zadanie3
    добавила задание 3

---
+++ задание 3/zadanie_three.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class zadanie_three {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, give me you name");
+        Scanner name = new Scanner(System.in);
+
+        System.out.println("Hello " + name.next());
+    }
+}
