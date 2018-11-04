99f52af366d2c992f785f40a39e51b041f9d7091
Дарья Борисова <d.borisova@MacBook-Pro-Dara.local>
2018-11-04 23:25:36 +0300

    story14
    Добавила 13 (не работает) и 14

---
+++ Урок 1/task13/task13.java
@@ -0,0 +1,18 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class task13 {
+
+    public static void main(String[] args) {
+
+   /*     outFio = new String[2];
+
+        String [] outFio = new String[2];
+
+        outFio[0] = 'Borisova";
+        outFio[1]= 'Daria';
+        outFio[2]='Yrievna';
+
+
+**/
+    }
+}
---
+++ Урок 1/task14/task14.java
@@ -0,0 +1,30 @@
+public class task14 {
+
+    public static void main(String[] args) {
+
+        zamena(2, 3);
+
+    }
+
+    public static void zamena(int a, int b) {
+
+        int za,zb;
+        zb= a;
+        za = b;
+
+
+        System.out.println("a = " + za);
+        System.out.println("b = " + zb);
+    };
+
+
+
+}
+/*
+    Integer a = 1;
+  *         Integer b = 2;
+          *         invoke(a, b);
+          *         System.out.println("a = " + a); // вернёт 2
+          *         System.out.println("b = " + b); // вернёт 1
+
+          **/
