eb83b50a6300a0de4b2e805c3c80626238d65b42
Дарья Борисова <d.borisova@MacBook-Pro-Dara.local>
2018-11-04 22:40:19 +0300

    zad_six
    Добавила решение 6

---
+++ Урок 1/задание 6/zadanie_six.java
@@ -0,0 +1,15 @@
+public class zadanie_six {
+
+    public static void main(String[] args) {
+
+        negativ(4);
+
+    }
+
+    public static void negativ(int chi) {
+
+        System.out.println(chi * (-1));
+    };
+
+}
+
