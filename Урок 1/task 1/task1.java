d80e877cf47e6359ce24084bee4cf4d1314df4d5
Дарья Борисова <d.borisova@MacBook-Pro-Dara.local>
2018-11-07 10:11:53 +0300

    task1
     * различается из-за создание конструктора без параметров - явно не указали

---
+++ Урок 1/task 1/task1.java
@@ -0,0 +1,14 @@
+public class task1 {
+
+    public static void main(String[] args) {
+
+        String msg = "Hello" + " " + "Bob!";
+
+        System.out.println(msg);
+    }
+
+}
+/**
+ * различается из-за создание конструктора без параметров - явно не указали
+
+ */
