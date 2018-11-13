cf6d0b2f56a559278ebbce5ae5b8e9031f999ea3
QA-Daria <43401586+QA-Daria@users.noreply.github.com>
2018-11-13 20:46:33 +0300

    Изменила задание 2

--- Урок 1/task2/Main.java
+++ Урок 1/task2/Main.java
@@ -1,9 +1,23 @@
 public class Main {
 
-    public static void main(String[] args) {
-       StringBuffer pereverni = new StringBuffer("переверни строку");
-        pereverni.reverse();
+    public static void main (String[] args) {
 
-        System.out.println(pereverni);
+        System.out.println(reverseWord("переверни строку"));
+
     }
+
+    public static StringBuffer reverseWord(String a){
+        StringBuffer slovo = new StringBuffer(a);
+        slovo.reverse();
+
+        return slovo;
+    }
+
+
 }
+
+/**
+ * Напишите метод который принимает String и возвращает данную строку в обратном порядке символов.
+ * Пример:
+ * 	reverseWord("переверни строку"); //вернёт "укотрс инревереп"
+ */
