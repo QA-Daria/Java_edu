d6ea631e177551e344b392e89cfcea248df648a7
QA-Daria <43401586+QA-Daria@users.noreply.github.com>
2018-11-13 21:07:11 +0300

    Изменила задание 3

--- Урок 1/task3/zadanie_three.java
+++ Урок 1/task3/zadanie_three.java
@@ -1,11 +1,20 @@
-import java.util.Scanner;
-
-public class zadanie_three {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, give me you name");
-        Scanner name = new Scanner(System.in);
-
-        System.out.println("Hello " + name.next());
+public class Main {
+    public static void main (String[] args) {
+    sayHello("");
+    sayHello("Олег");
+    sayHello("Денис");
+    }
+    public static String sayHello(String a){
+        System.out.println("Привет " + a +"!");
+        return a;
     }
 }
+
+/**
+ * Напишите метод который бы приветствовал человека по его имени, метод должен принимать один аргутмент String
+ * и выводить на консоль приветствие.
+ * Пример:
+ * 	sayHello("олег"); //вернёт "Привет Олег!"
+ * 	sayHello("ДЕНИС"); //вернёт "Привет Денис!"
+ * 	sayHello(""); //вернёт "Привет !"
+ */
